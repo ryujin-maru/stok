28d65e50d4703de1eb591188dfbfcb9880c9a562
徳永龍仁 <ryujin-maru@tokunagaryuuhitoshinoMacBook-Air.local>
2023-06-30 08:16:31 +0900

    sec02

---
+++ resources/views/components/elements/button.blade.php
@@ -0,0 +1,3 @@
+<div {{$attributes->merge(['class'=>"mb-8 text-center relative"])}} >
+    <button class="font-sub border px-8 py-2 border-gray-700 shadow-md cursor-pointer">See more</button>
+</div>
--- resources/views/stok/index.blade.php
+++ resources/views/stok/index.blade.php
@@ -6,36 +6,43 @@
 
         <div class="h-screen relative">
             <div class="absolute bottom-1/4 left-8 pr-4">
-                <h1 class="mb-4 text-lg text-shadow">そこに愛はあるんか</h1>
-                <p class="text-shadow">ここに説明文を追加します。長くてもいいようにレイアウトを設定します。</p>
+                <h1 class="tracking-wide mb-4 text-lg text-shadow">そこに愛はあるんか</h1>
+                <p class="tracking-wide text-shadow">ここに説明文を追加します。長くてもいいようにレイアウトを設定します。</p>
             </div>
         </div>
     </div>
 
 
-    <div id="container" class="flex justify-center pt-20 bg-white mb-20">
+    <div id="container" class="flex justify-center pt-20 bg-white mb-40">
         <div class="w-10/12">
             <section>
                 <x-section titleEn="Products" title="事業紹介"/>
 
                 <div class="mt-8 relative">
                     <img class="w-3/4" src="{{asset("images/test.jpg")}}"/>
-                    <div class="absolute border bg-white top-14 left-14 w-10/12  p-2">
+                    <div class="absolute border-t border-b bg-white top-14 left-10 w-11/12  p-2">
                         <h3 class="font-bold mb-2">なんか事業</h3>
-                        <p>ここに事業紹介の一つ目を書きます。何書こうかなマヨマヨ。あと少しなんか書かないと練習にならないからなーなんかない？</p>
+                        <p class="leading-7 font-medium">ここに事業紹介の一つ目を書きます。何書こうかなマヨマヨ。あと少しなんか書かないと練習にならないからなーなんかない？</p>
                     </div>
                 </div>
 
-                <div class="float-right mt-24 relative flex justify-end">
+                <div class="mt-32 relative flex justify-end">
                     <div class="w-3/4">
                         <img class="" src="{{asset("images/test.jpg")}}"/>
-                        <div class="absolute border bg-white top-14 right-14 w-10/12 p-2">
+                        <div class="absolute border-t border-b bg-white top-14 right-10 w-11/12 p-2">
                             <h3 class="font-bold mb-2">なんか事業</h3>
                             <p>ここに事業紹介の一つ目を書きます。何書こうかなマヨマヨ。あと少しなんか書かないと練習にならないからなーなんかない？</p>
                         </div>
                     </div>
                 </div>
+
+                <x-elements.button class="mt-40"/>
             </section>
+
+            <section>
+                <x-section titleEn="Members" title="メンバー"/>
+            </section>
+
         </div>
     </div>
 
